47dcb9cb735b16d2d7d273bf747abadcc37aa83c
Victor Brossa <brossajoao@gmail.com>
2025-06-04 01:38:07 -0400

    session routes

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { hash } from "bcrypt";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "Ok!" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("This e-mail is already registered.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
