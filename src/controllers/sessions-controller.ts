6cf485e73dac5542a7cc0b0d8d85c125d4b0a26e
Victor Brossa <brossajoao@gmail.com>
2025-06-05 14:49:27 -0400

    deliveries routes

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,41 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { authConfig } from "@/config/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "Ok!" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+    const { email, password } = bodySchema.parse(request.body);
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError("E-mail or password is not valid.", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail or password is not valid.", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({ role: user.role ?? "customer" }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const { password: hashedPassword, ...userWithoutPassword } = user;
+
+    return response.json({ token, ...userWithoutPassword });
   }
 }
 
