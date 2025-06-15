b7120df8884c9faebb0f363d212cba3735183516
Victor Brossa <brossajoao@gmail.com>
2025-06-14 22:44:01 -0400

    delivery

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
+import { log } from "console";
 import { Request, Response } from "express";
 import { z } from "zod";
 
@@ -17,9 +18,9 @@ class DeliveryLogController {
 
     if (!delivery) {
       throw new AppError("Delivery not found.", 404);
-    }
-
-    if (delivery.status === "processing") {
+    } else if (delivery.status === "delivered") {
+      throw new AppError("This order has already been delivered.");
+    } else if (delivery.status === "processing") {
       throw new AppError("Change status to shipped.");
     }
 
@@ -30,6 +31,30 @@ class DeliveryLogController {
       },
     });
     return response.status(201).json();
+  }
+
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      delivery_id: z.string().uuid(),
+    });
+
+    const { delivery_id } = paramsSchema.parse(request.params);
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+      include: {
+        logs: true,
+        user: true,
+      },
+    });
+
+    if (
+      request.user?.role === "customer" &&
+      request.user.id !== delivery?.userId
+    ) {
+      throw new AppError("Unknown delivery.", 401);
+    }
+
+    return response.json(delivery);
   }
 }
 
